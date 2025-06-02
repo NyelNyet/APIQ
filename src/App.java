5c0dd0e19a1c28c925c2ac758d10ec74361ba604
Danial <danialharith661@gmail.com>
2025-06-02 16:02:47 +0800

    Setup

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -10,7 +11,10 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        Button but1 = new Button("Hello");
+        but1.setOnAction(x -> System.out.println(but1.getText()));
         StackPane root = new StackPane();
+        root.getChildren().addAll(but1);
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
